23a8bd68eee68413a47f736fc6cdf5ac748c4bd7
David Schoenholtz <dmschoenholtz@gmail.com>
2018-06-27 16:32:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,7 @@ var playlist = {
   artist: "song"
 };
 
-function updatePlaylist()
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = 'songTitle'
+  return playlist
+}
